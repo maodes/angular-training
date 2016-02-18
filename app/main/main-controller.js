b84850cef6f3d8f128ee3e96e7c58484ca2b0855
Daniel Arbiol <arbiol.daniel0727@gmail.com>
2016-02-18 13:58:02 +0800

    fix: remove config object

--- app/main/main-controller.js
+++ app/main/main-controller.js
@@ -8,30 +8,6 @@
 		self.nameArr = [ 'faith', 'gomez' ];
 		self.addName = '';
 
-		var config = {
-			'name' : 'John Doe',
-			'email' : 'john.doe@mail.com',
-			'friends' : [
-				{
-					'name' : 'Peter',
-					'id' : '122-ad-001'
-				},
-				{
-					'name' : 'Peter',
-					'id' : '122-ad-001'
-				},
-				{
-					'name' : 'Peter',
-					'id' : '122-ad-001'
-				},
-				{
-					'name' : 'Peter',
-					'id' : '122-ad-001'
-				}
-			]
-		},
-		'enemies' : []
-
 		function addNameFn () {
 			self.nameArr.push( self.addName );
 		}
